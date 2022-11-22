5f31556df4ed2eb875f936124ba34c485717700b
suhjaesuk <suhjaesuk@gmail.com>
2022-11-22 14:37:21 +0900

    [프로그래머스] 나누어 떨어지는 숫자 배열

--- Programmers/P_나누어_떨어지는_숫자_배열.java
+++ Programmers/P_나누어_떨어지는_숫자_배열.java
@@ -1,2 +1,24 @@
-package Programmers;public class P_나누어_떨어지는_숫자_배열 {
+package Programmers;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class P_나누어_떨어지는_숫자_배열 {
+    class Solution {
+        public int[] solution(int[] arr, int d) {
+            int[] answer = {};
+            List<Integer> list = new ArrayList<>();
+            Arrays.sort(arr);
+
+            for(int i:arr){
+                if(i%d==0){
+                    list.add(i);
+                }
+            }
+            if (list.isEmpty()) {
+                list.add(-1);
+            }
+            answer = list.stream().mapToInt(u -> u).toArray();
+            return answer;
+        }
+    }
 }
