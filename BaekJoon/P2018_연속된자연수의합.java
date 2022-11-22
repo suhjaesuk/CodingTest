ae4c90226cc11db8e4d0d4d2d504b3d72a044d1c
suhjaesuk <suhjaesuk@gmail.com>
2022-11-22 14:31:21 +0900

    [백준] 연속된 자연수의 합

---
+++ BaekJoon/P2018_연속된자연수의합.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class P2018_연속된자연수의합 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+
+    int start = 1;
+    int end = 1;
+    int count = 1;
+    int sum = 1;
+
+    while(end!=N) {
+        if(sum>N) {
+            sum-=start;
+            start++;
+        }else if (sum==N) {
+            count++;
+            end++;
+            sum+=end;
+        }else {
+            end++;
+            sum+=end;
+        }
+    }
+        System.out.println(count);
+    }
+}
