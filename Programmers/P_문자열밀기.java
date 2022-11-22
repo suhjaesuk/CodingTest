0bbc55fe9d0f16d20579e6c61a6523c4ca7319b5
suhjaesuk <suhjaesuk@gmail.com>
2022-11-22 14:32:53 +0900

    [프로그래머스] 문자열 밀기

---
+++ Programmers/P_문자열밀기.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class P_문자열밀기 {
+    class Solution {
+        public int solution(String A, String B) {
+            int answer = 0;
+
+            String str = A;
+
+            for (int i = 0; i < A.length(); i++) {
+                if (str.equals(B)) {
+                    return answer;
+                }   String a = str.substring(str.length() - 1);
+                    str = a + str.substring(0, str.length() - 1);
+                    answer++;
+            }
+                return -1;
+
+        }
+
+    }
+}
+
