d96ce7fd22c5510607e762fb3833a7726dc9752a
suhjaesuk <suhjaesuk@gmail.com>
2023-05-12 16:27:15 +0900

    코딩테스트 풀이

--- BaekJoon/P1253_좋은수찾기.java
+++ BaekJoon/P1253_좋은수찾기.java
@@ -3,43 +3,39 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class P1253_좋은수찾기 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
-        for(int i=0; i<N; i++){
-            A[i]=Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int count =0;
 
-        for(int k=0; k<N; k++) {
-            int answer = A[k];
-            int i = 0;
-            int j = N-1;
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i],false);
+        }
 
-            while(i<j) {
-                if(A[i]+A[j]==answer) {
-                    if(i!=k && j!=k) {
-                        count++;
-                        break;
-                    }else if(i==k){
-                        i--;
-                    }else{
-                        j--;
+        int answer = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (map.containsKey(arr[i]+arr[j])) {
+                    if (map.get(arr[i]+arr[j]).equals(false)) {
+                        map.replace(arr[i]+arr[j], true);
+                        answer++;
                     }
-                }else if(A[i]+A[j]<answer){
-                    i++;
-                }else{
-                    j--;
-
                 }
             }
         }
-        System.out.println(count);
-        br.close();
+
+        System.out.println(answer);
     }
 }
--- BaekJoon/P2018_주몽의명령.java
+++ BaekJoon/P2018_주몽의명령.java
@@ -4,35 +4,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class P2018_주몽의명령 {
-    public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    int M = Integer.parseInt(br.readLine());
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int[] A = new int[N];
-    for(int i=0; i<N; i++) {
-        A[i]=Integer.parseInt(st.nextToken());
-    }
 
-    Arrays.sort(A);
-    int count=0;
-    int i=0;
-    int j=N-1;
-    while(i<j){
-        if (A[i]+A[j]<M) {
-            i++;
-        }else if(A[i]+A[j]>M) {
-            j--;
-        }else {
-            count ++;
-            j--;
-            i++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] sources = new int[n];
+        for (int i = 0; i < n; i++) {
+            sources[i] = Integer.parseInt(st.nextToken());
         }
-    }
-        System.out.println(count);
-        br.close();
+
+        Arrays.sort(sources);
+
+        int answer = 0;
+        int start = 0;
+        int end = sources.length - 1;
+
+        while (start <= end) {
+            if (sources[start] + sources[end] == m) {
+                answer++;
+                start++;
+                end--;
+            }
+            else if (sources[start] + sources[end] > m) {
+                end--;
+            }
+            else {
+                start++;
+            }
+        }
+
+        System.out.println(answer);
     }
 }
